05fa644212a6c4cb31501e4c42196a3338ebb4f4
466413559 <466413559@qq.com>
2017-03-14 18:26:25 +0800

    添加修改界面 tum修改

--- src/main/java/com/hust/jss/controller/TumController.java
+++ src/main/java/com/hust/jss/controller/TumController.java
@@ -48,7 +48,7 @@ public class TumController {
 		String stuId = "";
 		//验证当前用户是否还在线
 		if(stuName == null)
-			return "";
+			return "redirect:/";
 		else
 			stuId =  (String) request.getSession().getAttribute("id");
 		List<Task> taskList = new ArrayList<Task>();
@@ -92,7 +92,7 @@ public class TumController {
 		String stuId = "";
 		//验证当前用户是否还在线
 		if(stuName == null)
-			return "";
+			return "redirect:/";
 		else
 			stuId =  (String) request.getSession().getAttribute("id");
 		List<Task> taskList = new ArrayList<Task>();
